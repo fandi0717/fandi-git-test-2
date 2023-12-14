85ed72143e69fa2b1aa0cc489c39cf3379e86a79
fandi --git.dev <fandigit7@gmail.com>
2023-12-15 06:47:28 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur reprehenderit natus voluptatibus ducimus, est magnam, possimus error tempora nisi incidunt molestiae adipisci laudantium voluptatum, harum architecto expedita voluptates facilis nihil!</p>
+    <li>
+        
+    </li>
 </body>
 </html>
